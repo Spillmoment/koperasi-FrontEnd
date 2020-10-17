d89a0d6d8cdc3348327cf321730b0222a5b52e83
ackerman <do.crazy192@gmail.com>
2020-10-17 11:30:06 +0700

    include  vendor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,12 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
+
   }
 }
 </script>
